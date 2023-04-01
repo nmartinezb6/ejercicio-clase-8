fa12d1098fec4fcb26d4c0dc7df0a42f8a8c0d92
neryjm98 <neryjm98@gmail.com>
2023-04-01 09:44:47 -0600

    Agregar archivos de proyecto.

---
+++ p1c8/Program.cs
@@ -0,0 +1,46 @@
+﻿
+void matriz()
+{
+    string[] nombre = new string[]
+    {
+        "anderson", "brandon", "yeimi", "fatima","vivian"
+    };
+
+    foreach (string n in nombre)
+    {
+        Console.WriteLine("nombre=" + n);
+
+    }
+    int[] notas = new int[]
+    {
+        51, 80, 96,76,61
+    };
+
+    int promedio = 0;
+    foreach (int n in notas)
+    {
+        promedio = promedio + n; ;
+
+
+    }
+    promedio = promedio / notas.Length;
+    Console.WriteLine("el promedio es=" + promedio);
+
+    int notaMaxima = notas[0];
+    int indiceMaximo = 0;
+
+    for (int i = 1; i < notas.Length; i++)
+    {
+        if (notas[i] > notaMaxima)
+        {
+            notaMaxima = notas[i];
+            indiceMaximo = i;
+        }
+    }
+
+    string nombreNotaMaxima = nombre[indiceMaximo];
+    Console.WriteLine("La persona con la nota más alta es " + nombreNotaMaxima + " con una nota de " + notaMaxima);
+
+}
+
+matriz();
